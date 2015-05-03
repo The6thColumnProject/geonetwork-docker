d206ef22d6c7225bd9f4cd6cd10f79d7994eb84b
estani <you@example.com>
2015-05-03 17:38:52 +0000

    handly the python cache better

--- publisher/scripts/bootstrap.sh
+++ publisher/scripts/bootstrap.sh
@@ -6,6 +6,8 @@ gid=$HOST_USER_GROUP
 if [[ "$uid" && "$gid" ]]; then
     groupadd -fg $gid host_user_group 
     useradd -oMN -u $uid -g $gid host_user
+    mkdir /python-eggs && chown host_user python-eggs
+    export PYTHON_EGG_CACHE=/python-eggs
     sudo -Eu host_user "$@"
 else
     "$@"
